03a8b835ac55c66333c8371b92f746ba6a099afe
Malcolm Sparks <malcolm@congreve.com>
2013-07-13 14:15:10 +0100

    Added timbre logging

--- up-logging/project.clj
+++ up-logging/project.clj
@@ -1,12 +1,12 @@
 ;; Copyright © 2013, Malcolm Sparks. All Rights Reserved.
 ;;
 ;; The use and distribution terms for this software are covered by the
-;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) 
-;; which can be found in the file epl-v10.html at the root of this distribution.  
+;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file epl-v10.html at the root of this distribution.
 ;;
 ;; By using this software in any fashion, you are agreeing to be bound by the
 ;; terms of this license.
-;; 
+;;
 ;; You must not remove this notice, or any other, from this software.
 
 (read "project.clj")
@@ -17,6 +17,6 @@
   :url "http://github.com/malcolmsparks/up"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[up/up-core ~version]]
+  :dependencies [[up/up-core ~version]
+                 [com.taoensso/timbre "2.1.2"]]
   :up {:plugin up.logging/Logging})
-
--- up-logging/src/up/logging.clj
+++ up-logging/src/up/logging.clj
@@ -1,12 +1,13 @@
 (ns up.logging
   (:require
-   [lamina.core :refer (channel* join close receive-all) :as lamina])
+   [lamina.core :refer (channel* join close receive-all) :as lamina]
+   [taoensso.timbre :as timbre :refer (trace debug info warn error fatal spy)])
   (:import (up.start Plugin)))
 
 (def log (channel* :grounded? true))
 
 (defn console-logger [ev]
-  (println "UP>" (if-let [mt (meta ev)] (format "[meta: %s]" (str mt)) "") ev))
+  (debug (if-let [mt (meta ev)] (format "[meta: %s]" (str mt)) "") ev))
 
 (defrecord Logging [pctx]
   Plugin
